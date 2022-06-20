4f4a36d192564244bc2bcfd2ed63a4ea2bda2508
Sami Vauhkonen <101732172+Samivv@users.noreply.github.com>
2022-06-20 03:45:16 +0300

    ++++

--- code.js
+++ code.js
@@ -1,15 +1,23 @@
 const navActive = document.querySelectorAll(".nav ul li");
+const nav = document.querySelector(".nav");
+const burgerMenu = document.querySelector(".burger");
+const firstPage = document.querySelector(".first-page");
+
+burgerMenu.addEventListener("click", () => {
+    nav.style.visibility = "visible";
+    firstPage.addEventListener("click", () => {
+        nav.style.visibility = "hidden";
+    });
+});
+
 navActive[0].addEventListener("click", (e) => {
-    clearOtherButtons()
-    navActive[0].classList.add("active");
+    location.href = "index.html"
 });
 navActive[1].addEventListener("click", (e) => {
-    clearOtherButtons()
-    navActive[1].classList.add("active");
+    location.href = "showcase.html"
 });
 navActive[2].addEventListener("click", (e) => {
-    clearOtherButtons()
-    navActive[2].classList.add("active");
+    location.href = "contact.html"
 });
 
 function clearOtherButtons() {
@@ -17,3 +25,4 @@ function clearOtherButtons() {
     navActive[i].classList.remove("active");
     }
 }
+
