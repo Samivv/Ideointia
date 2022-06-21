489b117e8ff6ec9ebe7f12a75c14f8ad865b9903
Samivv <101732172+Samivv@users.noreply.github.com>
2022-06-22 00:17:00 +0300

    navigation improved

--- code.js
+++ code.js
@@ -2,6 +2,34 @@ const navActive = document.querySelectorAll(".nav ul li");
 const nav = document.querySelector(".nav");
 const burgerMenu = document.querySelector(".burger");
 const firstPage = document.querySelector(".first-page");
+const sections = document.querySelectorAll("section");
+
+const options = {
+    threshold: 0.7
+}
+let observer = new IntersectionObserver(navCheck, options);
+
+
+
+function navCheck(entries) {
+    
+    entries.forEach(entry => {
+        if(entry.target.getAttribute("id") == "first-page") {
+            clearOtherButtons();
+            navActive[0].classList.add("active");
+        } else if(entry.target.getAttribute("id") == "second-page") {
+            clearOtherButtons();
+            navActive[1].classList.add("active");
+        } else if(entry.target.getAttribute("id") == "third-page") {
+            clearOtherButtons();
+            navActive[2].classList.add("active");
+        }
+    });
+}
+navActive[0].classList.add("active");
+sections.forEach(section => {
+    observer.observe(section);
+})
 
 burgerMenu.addEventListener("click", () => {
     nav.classList.add(".nav-fade-in");
@@ -11,26 +39,16 @@ burgerMenu.addEventListener("click", () => {
     });
 });
 
-navActive[0].addEventListener("click", (e) => {
-    clearOtherButtons();
-    navActive[0].classList.add("active");
-
-    location.href = "#first-page"
-});
-navActive[1].addEventListener("click", (e) => {
-    clearOtherButtons();
-    navActive[1].classList.add("active");
-    location.href = "#second-page"
-});
-navActive[2].addEventListener("click", (e) => {
-    clearOtherButtons();
-    navActive[2].classList.add("active");
-    location.href = "#third-page"
-});
-
+for(let i=0;i<navActive.length;i++) {
+    navActive[i].addEventListener("click", (e) => {
+        clearOtherButtons();
+        // navActive[i].classList.add("active");
+        // location.href = "#"+sections[i].getAttribute("id");
+        sections[i].scrollIntoView({block: "start", behavior: "smooth"});
+    });
+}
 function clearOtherButtons() {
     for(let i=0;i<navActive.length;i++) {
     navActive[i].classList.remove("active");
     }
 }
-
